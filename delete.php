e66bb0accdbcaca0285f6c5d8ad34bbfe867a509
Henrique <101234830+HOR93@users.noreply.github.com>
2025-08-13 16:31:45 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Block projeto_transversal
+ *
+ * @package     block_projeto_transversal
+ * @copyright   2025 Henrique <@aluno.unb.br>
+ */
 require_once(__DIR__ . '/../../config.php');
 require_login();
 
@@ -17,4 +22,5 @@ delete_user($user);
 
 // Redireciona para logout com mensagem
 redirect(new moodle_url('/login/logout.php'), get_string('deletedmessage', 'local_deletando'));
+
 
